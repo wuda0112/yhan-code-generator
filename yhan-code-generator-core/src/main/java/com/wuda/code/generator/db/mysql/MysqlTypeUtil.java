2d522d054c5da7cd586026a289e6cbb33196b35a
wuda <wud@tututech.net>
2019-05-17 16:38:14 +0800

    当数据类型是BigInteger时，转换成Long

--- yhan-code-generator-core/src/main/java/com/wuda/code/generator/db/mysql/MysqlTypeUtil.java
+++ yhan-code-generator-core/src/main/java/com/wuda/code/generator/db/mysql/MysqlTypeUtil.java
@@ -13,17 +13,32 @@ public class MysqlTypeUtil {
     /**
      * mysql 数据类型对应的java类型.
      *
-     * @param fullMysqlTypeName
-     *         full MySQL type name
+     * @param fullMysqlTypeName full MySQL type name
+     * @param bigIntegerToLong  {@link MysqlType#BIGINT_UNSIGNED}对应的Java class type是{@link java.math.BigInteger},
+     *                          现实应用中应该还没必要,{@link Long}类型即可.如果是true,则表示如果
+     *                          数据库中的定义是{@link MysqlType#BIGINT_UNSIGNED},则转换成{@link MysqlType#INT_UNSIGNED}.
      * @return java class
      */
-    public static Class<?> mysqlTypeToJavaType(String fullMysqlTypeName) {
+    public static Class<?> mysqlTypeToJavaType(String fullMysqlTypeName, boolean bigIntegerToLong) {
         MysqlType mysqlType = MysqlType.getByName(fullMysqlTypeName);
         try {
+            if (mysqlType == MysqlType.BIGINT_UNSIGNED && bigIntegerToLong) {
+                mysqlType = MysqlType.INT_UNSIGNED;
+            }
             return Class.forName(mysqlType.getClassName());
         } catch (ClassNotFoundException e) {
             // 使用的是mysql java驱动的类,不会来到这里
             throw new CodeGenerateException(e);
         }
+    }
+
+    /**
+     * mysql 数据类型对应的java类型.
+     *
+     * @param fullMysqlTypeName full MySQL type name
+     * @return java class
+     */
+    public static Class<?> mysqlTypeToJavaType(String fullMysqlTypeName) {
+        return mysqlTypeToJavaType(fullMysqlTypeName, true);
     }
 }
